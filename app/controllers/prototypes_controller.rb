356c6970cd2f667a249a4fe29fee5c26894d1bc4
s-kent <s.kento0210@gmail.com>
2021-04-27 13:42:34 +0900

    投稿機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit]
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @prototypes = Prototype.all
@@ -18,15 +17,16 @@ class PrototypesController < ApplicationController
     else
       render :new
     end
-  
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
@@ -47,11 +47,7 @@ class PrototypesController < ApplicationController
 
   private
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
-  end
-
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
